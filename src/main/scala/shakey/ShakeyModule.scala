4e4b8a03c1f114aee9acd52d92064593747e55bf
Jun Tsai <jcai@ganshane.com>
2014-09-28 14:37:08 +0800

    FIXES #11 创建表结构

--- src/main/scala/shakey/ShakeyModule.scala
+++ src/main/scala/shakey/ShakeyModule.scala
@@ -1,13 +1,15 @@
 package shakey
 
+import scala.slick.driver.H2Driver.simple._
 import shakey.config.ShakeyConfig
 import scala.io.Source
 import org.apache.tapestry5.ioc.annotations.{Contribute,Symbol}
 import org.apache.tapestry5.ioc.services.{FactoryDefaults, SymbolProvider}
 import org.apache.tapestry5.ioc.{ServiceBinder, MappedConfiguration}
-import shakey.internal.{MessageNotifierService, MemoryStockDatabase, RealtimeMktDataFetcher, XmlLoader}
+import shakey.internal._
 import com.ib.controller.ApiController
 import shakey.services.{StockDatabase, ShakeyClient}
+import scala.Some
 
 /**
  * Created by jcai on 14-9-25.
@@ -21,6 +23,19 @@ object ShakeyModule {
   def buildApiController(config:ShakeyConfig):ApiController={
     ShakeyClient.start(config)
   }
+
+  def buildDatabase(@Symbol(ShakeyConstants.SERVER_HOME) serverHome: String): Database = {
+    val stocks: TableQuery[Stocks] = TableQuery[Stocks]
+    val db = Database.forURL("jdbc:h2:mem:hello", driver = "org.h2.Driver")
+    //创建表结构
+    db.withSession {
+      implicit session =>
+        stocks.ddl.createStatements
+    }
+   
+    db
+  }
+
   def buildShakeConfig(@Symbol(ShakeyConstants.SERVER_HOME) serverHome:String):ShakeyConfig={
     val filePath = serverHome + "/config/shakey.xml"
     val content = Source.fromFile(filePath, ShakeyConstants.UTF8_ENCODING).mkString
