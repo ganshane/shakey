7abaab007434c26bbcd4d28257cc055e1eabd7fb
Jun Tsai <jcai@ganshane.com>
2014-09-28 15:36:03 +0800

    Revert "FIXES #11 创建表结构"
    This reverts commit 4e4b8a03c1f114aee9acd52d92064593747e55bf.

--- src/main/scala/shakey/ShakeyModule.scala
+++ src/main/scala/shakey/ShakeyModule.scala
@@ -1,15 +1,13 @@
 package shakey
 
-import scala.slick.driver.H2Driver.simple._
 import shakey.config.ShakeyConfig
 import scala.io.Source
 import org.apache.tapestry5.ioc.annotations.{Contribute,Symbol}
 import org.apache.tapestry5.ioc.services.{FactoryDefaults, SymbolProvider}
 import org.apache.tapestry5.ioc.{ServiceBinder, MappedConfiguration}
-import shakey.internal._
+import shakey.internal.{MessageNotifierService, MemoryStockDatabase, RealtimeMktDataFetcher, XmlLoader}
 import com.ib.controller.ApiController
 import shakey.services.{StockDatabase, ShakeyClient}
-import scala.Some
 
 /**
  * Created by jcai on 14-9-25.
@@ -23,19 +21,6 @@ object ShakeyModule {
   def buildApiController(config:ShakeyConfig):ApiController={
     ShakeyClient.start(config)
   }
-
-  def buildDatabase(@Symbol(ShakeyConstants.SERVER_HOME) serverHome: String): Database = {
-    val stocks: TableQuery[Stocks] = TableQuery[Stocks]
-    val db = Database.forURL("jdbc:h2:mem:hello", driver = "org.h2.Driver")
-    //创建表结构
-    db.withSession {
-      implicit session =>
-        stocks.ddl.createStatements
-    }
-   
-    db
-  }
-
   def buildShakeConfig(@Symbol(ShakeyConstants.SERVER_HOME) serverHome:String):ShakeyConfig={
     val filePath = serverHome + "/config/shakey.xml"
     val content = Source.fromFile(filePath, ShakeyConstants.UTF8_ENCODING).mkString
--- src/main/scala/shakey/internal/Tables.scala
+++ src/main/scala/shakey/internal/Tables.scala
@@ -3,8 +3,8 @@ package shakey.internal
 import scala.slick.driver.H2Driver.simple._
 import scala.slick.lifted.{ProvenShape, Column, Tag}
 
-class Stocks(tag: Tag)
-extends Table[(String, Double, Long)](tag, "STOCKS") {
+class StockTable(tag: Tag)
+  extends Table[(String, Double, Long)](tag, "STOCKS") {
   def symbol: Column[String] = column[String]("SYMBOL", O.PrimaryKey)
 
   def ratePerSec: Column[Double] = column[Double]("RATE_PER_SEC")
