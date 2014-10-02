5634448f9110d996e7f5aff8b943187aee5aa62a
Jun Tsai <jcai@ganshane.com>
2014-10-02 20:56:02 +0800

    修正错误的语句，导致不能启动

--- src/main/scala/shakey/internal/ShakeyClient.scala
+++ src/main/scala/shakey/internal/ShakeyClient.scala
@@ -8,16 +8,16 @@ import shakey.services.LoggerSupport
 /**
  * shakey client
  */
-object ShakeyClient extends LoggerSupport{
+object ShakeyClient extends LoggerSupport {
   def start(config: ShakeyConfig, screen: ShakeySplashScreen): ApiController = {
     val controller = new ApiController(new ShakeyConnectionHandler(screen),
       new ApiLogger("in"),
       new ApiLogger("out"))
-    controller.connect(config.ibApiHost,config.ibApiPort,1024)
-    controller.disconnect()
+    controller.connect(config.ibApiHost, config.ibApiPort, 1024)
     controller
   }
-  class ApiLogger(val prefix:String) extends ApiConnection.ILogger{
+
+  class ApiLogger(val prefix: String) extends ApiConnection.ILogger {
     def log(str: String): Unit = {
       //logger.debug(prefix+":{}",str)
     }
@@ -35,16 +35,17 @@ object ShakeyClient extends LoggerSupport{
     def accountList(list: java.util.ArrayList[String]): Unit = ()
 
     def error(e: Exception): Unit = {
-      logger.error(e.getMessage,e)
+      logger.error(e.getMessage, e)
     }
 
     def message(id: Int, errorCode: Int, errorMsg: String): Unit = {
       screen.setSysMessage("code:" + errorCode + new String(errorMsg.getBytes("GBK"), "UTF8"))
-      logger.error("ID:"+id+" CODE:{},msg:{}",errorCode,new String(errorMsg.getBytes("GBK"),"UTF8"))
+      logger.error("ID:" + id + " CODE:{},msg:{}", errorCode, new String(errorMsg.getBytes("GBK"), "UTF8"))
     }
 
     def show(string: String): Unit = {
-      logger.info("show:{}",string)
+      logger.info("show:{}", string)
     }
   }
+
 }
