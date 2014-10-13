de5216e270e845ffb73b964dbbea133a83d8ecf3
Jun Tsai <jcai@ganshane.com>
2014-10-13 15:45:35 +0800

    FIXES #32 完成触碰R20股票的分析，并且按照强度进行排序

--- shakey-server/src/main/resources/r20.ftl
+++ shakey-server/src/main/resources/r20.ftl
@@ -10,6 +10,6 @@ categories: stock
  <td>强度指数</td>
 </tr>
 <#list stocks as s>
-  <tr><td><a href="http://stock.finance.sina.com.cn/usstock/quotes/${s.symbol}.html" target="_blank">${s.symbol}</a></td><td>${s.strongRate}</td></tr>
+  <tr><td><a href="http://stock.finance.sina.com.cn/usstock/quotes/${s.symbol()}.html" target="_blank">${s.symbol()}</a></td><td>${s.strongRate()}</td></tr>
 </#list>
 </table>
---
+++ shakey-server/src/main/scala/shakey/server/internal/analyzer/R20Analyzer.scala
@@ -0,0 +1,45 @@
+package shakey.server.internal.analyzer
+
+import shakey.server.services.StockAnalyzer
+import org.apache.tapestry5.json.{JSONArray, JSONObject}
+import shakey.server.internal.stat.StockAlgorithm
+import java.util
+import scala.collection.mutable.ListBuffer
+
+/**
+ * 分析触碰到R20的股票
+ */
+class R20Analyzer extends StockAnalyzer {
+  def processStockDataInOneYear(symbol: String, data: JSONArray) {
+    val pos: Int = data.length - 1
+    val r20 = 0.until(20).
+      map(x => data.getJSONObject(pos - x).
+      getDouble("c")).sum / 20
+
+    val current: JSONObject = data.getJSONObject(pos)
+    val o = current.getDouble("o")
+    val c = current.getDouble("c")
+    val isReach = (Math.abs(o - r20) + Math.abs(r20 - c)) == Math.abs(c - o)
+    if (isReach) {
+      list += new R20Stock(symbol, StockAlgorithm.calStrongRate(data, 25))
+    }
+  }
+
+  def getTemplatePath: String = {
+    return "/r20.ftl"
+  }
+
+  def addDataToTemplateAfterAnalysis(model: util.HashMap[AnyRef, AnyRef]) {
+    val arr = list.sorted.toArray
+    model.put("stocks", arr)
+  }
+
+  private val list = new ListBuffer[R20Stock]()
+
+  class R20Stock(val symbol: String, val strongRate: Double) extends Comparable[R20Stock] {
+    def compareTo(o: R20Stock): Int = {
+      o.strongRate.compareTo(strongRate)
+    }
+  }
+
+}
